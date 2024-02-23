8501d0434699038d79fc3c63ee7cce90dcb29354
Pranjal Ashok Singh <pranjalsingh.ak@gmail.com>
2024-02-23 20:38:11 +0530

    60_strings

---
+++ 03_JavaScript/60_string.js
@@ -0,0 +1,27 @@
+console.log("This is strings tutorial");
+let a = "Pranjal";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+console.log(a[6]);
+// console.log(a[7]);
+
+console.log(a.length);
+
+let name1 = "Pranjal";
+let name2 = "Shravani"
+console.log("Name1 is "+name1+" and Name 2 is "+name2);
+console.log(`Name1 is ${name1} and Name2 is ${name2}`);
+
+let g = "Harsh";
+console.log(g.toUpperCase());
+console.log(g.toLowerCase());
+console.log(g.slice(1,4));
+console.log(g.slice(1));
+console.log(g.replace("a", "@"));
+console.log(g.concat(name2));
+console.log(g.concat(name2, "Pranjal", "Chiku"));
+console.log(g);
